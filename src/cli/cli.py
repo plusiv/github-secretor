4c766b2c9a125241ebfe79b0b5ad8a1c68b28e6b
plusiv <jorge.massih@MacBook-Pro-de-Bastardo-3.local>
2023-05-01 12:23:51 -0400

    feat: add orgs subcommand

--- src/cli/cli.py
+++ src/cli/cli.py
@@ -4,4 +4,7 @@ from rich.console import Console
 
 err_console = Console(stderr=True)
 app = typer.Typer()
+
+# Add sub commands
 app.add_typer(repos_cli.app, name='repos')
+app.add_typer(repos_cli.app, name='orgs')
